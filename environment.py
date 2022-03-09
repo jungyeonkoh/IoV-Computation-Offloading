dbae44bae255d981adb31496640ebaa852274ba1
jungyeonkoh <jwe06020@naver.com>
2022-03-09 18:05:18 +0900

    calculate_reward() 완료

--- environment.py
+++ environment.py
@@ -1,12 +1,15 @@
 import numpy as np
 import pandas as pd
+import math
 
 VEH_COMP_RESOURCE = 500 #(MHz)
 VEH_TRAN_POWER = 0.1 #(W)
 VEC_COMP_RESOURCE = 6300 #(MHz)
 VEC_POWER = 0.007 #(W)
 BANDWIDTH = 5 #(MHz)
-#KAPPA(fixed)
+PATH_FADE = 3.75
+KAPPA = 10 ** -5 #원래 10^-11~10^-27 (결과에 따라 scaling할것)
+#NOISE =
 
 class Vehicle:
     def __init__(self, id, distance, velocity):
@@ -30,7 +33,7 @@ class Server:
         self.pos = [pos_x, pos_y]
         self.comp = np.random.normal(VEC_COMP_RESOURCE, 70)
         self.power = np.random.normal(VEC_POWER, 0.002)
-        self.crowd = 0 #init as 0 (N_j)
+        self.crowd = 1 #init as 1 (N_j)
 
 class Env:
     def __init__(self, nv, ns, load_vehicle_position, load_task_position, load_server_position):
@@ -111,12 +114,38 @@ class Env:
             state_vector.append(state_vector_by_vehicle)
         return state_vector
 
-    def calculate_reward(self, vehicle, action): # 논문 수정하
+    def get_max_tolerance(self, v, s): # Eq 1,2 # ID starts from 1
+        stay_time = 2 * self.vehicles[v-1].distance[s-1] / self.vehicles[v-1].v
+        return min(stay_time, self.tasks[v-1].threshold)
+
+    def get_transmission_rate(self, v, s):
+        shared_bandwidth = BANDWIDTH / self.servers[s-1].crowd
+        log = self.vehicles[v-1].tran * (self.vehicles[v-1].distance[s-1] ** (-PATH_FADE))
+        log /= self.servers[s-1].crowd #todo: white noise ref 찾아보기
+        return shared_bandwidth * math.log2(log+1)
+
+    def get_local_computing(self, v):
+        time = self.tasks[v-1].comp / self.vehicles[v].comp
+        energy = KAPPA * (self.vehicles[v].comp ** 2) * self.tasks[v-1].comp
+        return time, energy
+
+    def get_remote_computing(self, v, s):
+        trans = self.tasks[v-1].input / self.get_transmission_rate(v,s)
+        comp = self.tasks[v-1].comp / (self.servers[s-1].comp / self.servers[s-1].crowd)
+        time = trans + comp
+        energy = self.vehicles[v-1].trans * trans + self.servers[s-1].power * comp
+        return time, energy
+
+    def calculate_reward(self, vehicle, action): # 논문 수정하기 / 수식 이상함
         """
         Calculates the reward based on the action of the vehicle.
         """
-        reward = self.tasks[vehicle].threshold #TODO
-        return reward
+        reward = self.get_max_tolerance(vehicle, action[1])
+        local_time, local_energy = self.get_local_computing(vehicle)
+        remote_time, remote_energy = self.get_remote_computing(vehicle, action[1])
+        time = (1-self.tasks[vehicle].e_weight) * (action[0] * local_time + (1-action[0]) * remote_time)
+        energy = self.tasks[vehicle].e_weight * (action[0] * local_energy + (1-action[0]) * remote_energy)
+        return reward - time - energy
 
     def train_step(self):
         """
@@ -130,7 +159,7 @@ class Env:
         self.update_vehicle()
         self.update_task()
         state = self.construct_state()
-        action = np.zeros((self.num_vehicle, 3)) # 논문 수정하기: action = [float, float, int] (vehicle, server, #server)
+        action = np.zeros((self.num_vehicle, 2)) # 논문 수정하기: action = [float, int] (vehicle, #server)
         for v in range(self.num_vehicles):
             #action[v] = model.infer_action() #TODO
             rews[v] = self.calculate_reward(v, action[v])
